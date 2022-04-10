95902f3eb62689530656b54bbd23ef7248ad9fee
Rizky Rafi Azhara <azhararizkyrafi@gmail.com>
2022-04-11 06:08:32 +0700

    fix audio size in mobile breakpoint

--- src/components/Surah.vue
+++ src/components/Surah.vue
@@ -11,7 +11,7 @@ export default {
             <p class="text-right text-4xl font-alqalam">{{ arab }}</p>
         </div>
         <p class="mt-5"><span class="font-bold">Artinya:</span> {{ translation }}</p>
-        <audio controls class="mt-3">
+        <audio controls class="mt-3 w-full sm:w-auto">
             <source :src="audio" type="audio/mp3">
             Your browser does not support the audio element.
         </audio>
