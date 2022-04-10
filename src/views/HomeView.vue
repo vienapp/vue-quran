e148ed6dd8f20a8aad7a5dff23e0e4c3a074ae02
Rizky Rafi Azhara <azhararizkyrafi@gmail.com>
2022-04-10 13:52:02 +0700

    add loading overlay

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,24 @@
 <script>
 import Card from '../components/Card.vue'
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
 
 export default {
     components: {
-        Card
+        Card, Loading
     },
     data() {
         return {
-            surahs: []
+            surahs: [],
+            loading: true,
+            fullPage: false
         }
     },
     async created() {
         const response = await fetch("https://quran-api-id.vercel.app/surahs");
         const result = await response.json();
         this.surahs = result.data;
+        this.loading = false
         console.log(this.surahs)
     }
 }
@@ -21,7 +26,8 @@ export default {
 
 <template>
     <div class="container mx-auto px-5 md:px-5 lg:px-0">
-        <div class="my-10 grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <loading v-model:active="loading" :is-full-page="fullPage" loader="dots" background-color="#181818" color="#50c4cf" />
+        <div v-if="loading === false" class="my-10 grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
             <Card v-for="surah in surahs" :key="surah.number" :name="surah.name" :number="surah.number"
                 :revelation="surah.revelation" :numberAyahs="surah.numberOfAyahs" :surahNumber="surah.number" />
         </div>
--- src/views/SurahView.vue
+++ src/views/SurahView.vue
@@ -1,18 +1,23 @@
 <script>
 import Surah from '../components/Surah.vue'
 import Tafsir from '../components/Tafsir.vue'
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
 
 export default {
     components: {
         Surah,
-        Tafsir
+        Tafsir,
+        Loading
     },
     data() {
         return {
             surahs: [],
             bismillah: [],
             tabs: ['Surah', 'Tafsir'],
-            currentTab: 'Surah'
+            currentTab: 'Surah',
+            loading: true,
+            fullPage: false
         }
     },
     computed: {
@@ -28,42 +33,44 @@ export default {
         const response = await fetch("https://quran-api-id.vercel.app/surahs/" + number);
         const result = await response.json();
         this.surahs = result.data;
+        this.loading = false
         this.bismillah = this.surahs.bismillah
     }
 }
 </script>
 
 <template>
-    <div class="container mx-auto px-5 md:px-5 lg:px-0">
-        <div class="text-md mt-2 breadcrumbs">
-            <ul>
-                <li>
-                    <router-link to="/">Beranda</router-link>
-                </li>
-                <li>{{ surahs.name }}</li>
-            </ul>
-        </div>
-        <router-link to="/" class="btn btn-sm gap-2 btn-ghost normal-case mt-2 px-0">
-            ❮ Kembali ke beranda
-        </router-link>
-        <div class="mt-4">
-            <h1 class="text-4xl font-bold">{{ surahs.name }}</h1>
-            <p class="text-md">{{ surahs.translation }}</p>
-        </div>
-        <div class="tabs mt-5">
-            <a class="tab tab-lg tab-bordered" v-for="tab in tabs" :key="tab"
-                :class="{ 'tab-active': currentTab === tab}" @click="currentTab = tab">{{tab}}</a>
-        </div>
-        <div class="card bg-base-100 shadow-xl mb-5">
-            <div class="card-body">
-                <p v-if="currentTab === 'Surah'" class="text-center text-4xl font-alqalam">{{ bismillah.arab }}</p>
-                <div v-if="currentTab === 'Surah'" class="divider"></div>
-                <keep-alive>
-                    <component :is="currentTabComponent" v-for="surah in surahs.ayahs" :key="surah"
-                        :number="surah.number.inSurah" :arab="surah.arab" :translation="surah.translation"
-                        :tafsir="surah.tafsir.kemenag.short" />
-                </keep-alive>
-            </div>
+<loading v-model:active="loading" :is-full-page="fullPage" loader="dots" background-color="#181818" color="#50c4cf" />
+<div v-if="loading === false" class="container mx-auto px-5 md:px-5 lg:px-0">
+    <div class="text-md mt-2 breadcrumbs">
+        <ul>
+            <li>
+                <router-link to="/">Beranda</router-link>
+            </li>
+            <li>{{ surahs.name }}</li>
+        </ul>
+    </div>
+    <router-link to="/" class="btn btn-sm gap-2 btn-ghost normal-case mt-2 px-0">
+        ❮ Kembali ke beranda
+    </router-link>
+    <div class="mt-4">
+        <h1 class="text-4xl font-bold">{{ surahs.name }}</h1>
+        <p class="text-md">{{ surahs.translation }}</p>
+    </div>
+    <div class="tabs mt-5">
+        <a class="tab tab-lg tab-bordered" v-for="tab in tabs" :key="tab" :class="{ 'tab-active': currentTab === tab}"
+            @click="currentTab = tab">{{tab}}</a>
+    </div>
+    <div class="card bg-base-100 shadow-xl mb-5">
+        <div class="card-body">
+            <p v-if="currentTab === 'Surah'" class="text-center text-4xl font-alqalam">{{ bismillah.arab }}</p>
+            <div v-if="currentTab === 'Surah'" class="divider"></div>
+            <keep-alive>
+                <component :is="currentTabComponent" v-for="surah in surahs.ayahs" :key="surah"
+                    :number="surah.number.inSurah" :arab="surah.arab" :translation="surah.translation"
+                    :tafsir="surah.tafsir.kemenag.short" />
+            </keep-alive>
         </div>
     </div>
+</div>
 </template>
