2741e943d9ca25c54fd6dc9d8ac8954789aa3c17
Rizky Rafi Azhara <azhararizkyrafi@gmail.com>
2022-04-15 19:56:54 +0700

    add error handling for suspense component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,33 @@
 <script>
+import { ref, onErrorCaptured } from 'vue'
 import Card from '../components/Card.vue'
 import Loading from '../components/Loading.vue'
 
 export default {
     components: {
-        Card, Loading
+        Card,
+        Loading
+    },
+    setup() {
+        const error = ref();
+        
+        onErrorCaptured((e) => {
+            error.value = e;
+            return true;
+        });
+
+        return {
+            error
+        }
     }
 }
 </script>
 
 <template>
     <div class="container mx-auto px-5 md:px-5 lg:px-0">
-        <Suspense>
+        <div v-if="error" class="flex justify-center items-center h-[80vh] text-3xl text-center">Data gagal dimuat,
+            silahkan coba lagi.</div>
+        <Suspense v-else>
             <Card />
             <template #fallback>
                 <div class="flex justify-center items-center h-[80vh]">
